e4191e814f97fdd4c3739f5be807f6feb2bff142
Katelyn Rains <KRains@tql.com>
2021-06-29 17:02:24 -0400

    final commit 629

--- src/app/product/product-detail/product-detail.component.ts
+++ src/app/product/product-detail/product-detail.component.ts
@@ -1,4 +1,8 @@
 import { Component, OnInit } from '@angular/core';
+import { ActivatedRoute, Router } from '@angular/router';
+import { Vendor } from 'src/app/vendor/vendor.class';
+import { Product } from '../product.class';
+import { ProductService } from '../product.service';
 
 @Component({
   selector: 'app-product-detail',
@@ -6,10 +10,32 @@ import { Component, OnInit } from '@angular/core';
   styleUrls: ['./product-detail.component.css']
 })
 export class ProductDetailComponent implements OnInit {
+  product: Product = new Product();
+  vendor: Vendor = new Vendor();
+  id: number = 0;
 
-  constructor() { }
+  constructor(
+    private prodsvc: ProductService,
+    private route: ActivatedRoute,
+    private router: Router
+  ) { }
 
+deleteProd(): void {
+  this.id = this.route.snapshot.params.id;
+  this.prodsvc.remove(this.product).subscribe(
+    res => {console.debug("delete successful");
+            this.router.navigateByUrl("/home");},
+    err => {console.error(err);}        
+  );}
   ngOnInit(): void {
+    this.id = this.route.snapshot.params.id;
+    this.prodsvc.get(this.id).subscribe(
+      res => {
+        console.debug("Product:", res);
+        this.product = res;
+      },
+      err => {console.error(err);}
+    )
   }
 
 }
--- src/app/product/product-edit/product-edit.component.ts
+++ src/app/product/product-edit/product-edit.component.ts
@@ -6,6 +6,7 @@ import { Component, OnInit } from '@angular/core';
   styleUrls: ['./product-edit.component.css']
 })
 export class ProductEditComponent implements OnInit {
+  prod: Product = new Product();
 
   constructor() { }
 
